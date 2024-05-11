825c76120224944d67f054d9a937cf68c6606a3a
richardljohn <richardljohn@gmail.com>
2024-05-11 17:43:28 -0400

    Deployment

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,44 +1,44 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "../styles/navigation.css";
 
 function Navigation() {
-  const navRef = useRef();
-  const [isNavOpen, setIsNavOpen] = useState(false);
+	const navRef = useRef();
 
-  const toggleNavigation = () => {
-    setIsNavOpen(!isNavOpen);
-  };
+	const showNavigation = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
 
-  const closeNavigation = () => {
-    setIsNavOpen(false);
-  };
+	return (
+		<header>
+			<h2><Link className="logo" to="/pokedot">Pokédot</Link></h2>
+			<nav ref={navRef}>
+				{/* <a className="nav-link" href="/search">Search</a>
+        		<a className="nav-link" href="/profile">Profile</a>  */}
 
-  return (
-    <header>
-      <h2>
-        <Link className="logo" to="/pokedot">
-          Pokédot
-        </Link>
-      </h2>
-      <nav ref={navRef} className={isNavOpen ? "responsive_nav" : ""}>
-        <Link to="/pokedot/pokemon" onClick={closeNavigation}>
-          Pokédex
-        </Link>
-        <Link to="/pokedot" onClick={closeNavigation}>
-          Disclaimer
-        </Link>
-        <button className="nav-btn nav-close-btn" onClick={toggleNavigation}>
-          <FaTimes />
-        </button>
-      </nav>
 
-      <button className="nav-btn" onClick={toggleNavigation}>
-        <FaBars />
-      </button>
-    </header>
-  );
+				{/* Might need to add a new html or index file */}
+				<Link className="nav-link" to="/pokedot/pokemon">Pokédex</Link> 
+				<Link className="nav-link" to="/pokedot">Disclaimer</Link> 
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavigation}>
+					<FaTimes />
+				</button>
+			</nav>
+
+			{/* Redo when Profile, Accounts, Favorites, Teams, etc. are done with. */}
+
+			<button
+				className="nav-btn"
+				onClick={showNavigation}>
+				<FaBars />
+			</button>
+		</header>
+	);
 }
 
 export default Navigation;
