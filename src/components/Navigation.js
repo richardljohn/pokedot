c80d81571e2c79486bc297551439daa08708d367
richardljohn <richardljohn@gmail.com>
2024-05-11 17:23:01 -0400

    Deployment

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -14,16 +14,15 @@ function Navigation() {
 
 	return (
 		<header>
-			<h2><a className="logo" href="/pokedot">Pokédot</a></h2>
+			<h2><Link className="logo" to="/pokedot">Pokédot</Link></h2>
 			<nav ref={navRef}>
 				{/* <a className="nav-link" href="/search">Search</a>
         		<a className="nav-link" href="/profile">Profile</a>  */}
 
 
 				{/* Might need to add a new html or index file */}
-				<Link></Link>
-				<a className="nav-link" href="/pokedot/pokemon">Pokédex</a> 
-				<a className="nav-link" href="/pokedot">Disclaimer</a> 
+				<Link className="nav-link" to="/pokedot/pokemon">Pokédex</Link> 
+				<Link className="nav-link" to="/pokedot">Disclaimer</Link> 
 				<button
 					className="nav-btn nav-close-btn"
 					onClick={showNavigation}>
